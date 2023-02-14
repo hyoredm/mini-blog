8ebcb2aa75414bd779d53646017e1041943c43a7
hyoredm <hyoredm@gmail.com>
2023-02-14 23:55:53 +0900

    commit

---
+++ src/component/page/PostViewPage.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import CommentList from "../list/CommentList"
+import TextInput from "../ui/TextInput";
+import Button from "../ui/Button";
+import data from "../../data.json";
+
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width: 720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-buttom: 16px;
+        }
+    }
+`;
+
+const PostContainer = styled.div`
+    padding: 8px 16px;
+    border: 1px solid grey;
+    border-radius: 8px;
+`;
+
+const TitleText = styled.p`
+    font-size: 28px;
+`
